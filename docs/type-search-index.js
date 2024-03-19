4b6483d56a7efdebd87005c000851ae2dcf6762c
HaolinZheng <hzheng@elpuig.xeill.net>
2024-03-19 09:42:45 +0100

    Casi

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"model","l":"Classs"},{"p":"controller","l":"ClasssController"},{"p":"<Unnamed>","l":"Main"},{"p":"view","l":"Menu"},{"p":"model","l":"Operator"},{"p":"controller","l":"OperatorController"},{"p":"model","l":"Skill"},{"p":"controller","l":"SkillController"}];updateSearchResults();
